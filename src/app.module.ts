d7518c0ae4fdb8f7c32d75ecf5c7698e69b06f7b
Maxim Marchenko <maxim.marchenko.bp@gmail.com>
2024-05-03 22:06:01 +0300

    added typeorm package. connected to users database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { UserService } from './user.service';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './interceptors/http-exception.filter';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'vfrc_SHADIQ_1989',
+      entities: [],
+      database: 'users',
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [
     UserService,
